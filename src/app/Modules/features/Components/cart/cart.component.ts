4f611c69eab29ffc85ceee8723a1543afb7dbd0b
binda-nabendu <bindanabendu@gmail.com>
2024-03-27 23:30:22 +0600

    fix bux connect api to cart

--- src/app/Modules/features/Components/cart/cart.component.ts
+++ src/app/Modules/features/Components/cart/cart.component.ts
@@ -11,13 +11,20 @@ import {AppState} from "../../../../Models/AppState";
 })
 export class CartComponent {
   cart_item: any;
+
+  totalPrice=0;
+  discountPrice = 0;
   constructor(private router : Router, private cartService: CartService, private store : Store<AppState>) {
   }
   ngOnInit(){
     this.cartService.getCartService();
     this.store.pipe(select((store) => store.cart)).subscribe((cart)=>{
       this.cart_item = cart.cartItems;
-      console.log("Cart data", cart.cartItems);
+      for(let i of this.cart_item){
+        this.totalPrice += i.price;
+        this.discountPrice += i.discountedPrice;
+      }
+      console.log("Cart data", this.cart_item);
     })
   }
   goToCheckout(){
