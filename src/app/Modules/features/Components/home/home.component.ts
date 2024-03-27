593be5555edf7e094973e5eb14a4f462b85c2553
binda-nabendu <bindanabendu@gmail.com>
2024-03-27 23:36:00 +0600

    fix error issue of home but till now did not connect all section to api due to undefined issue

--- src/app/Modules/features/Components/home/home.component.ts
+++ src/app/Modules/features/Components/home/home.component.ts
@@ -4,6 +4,9 @@ import {gounsPage1} from "../../../../../Data/Gouns/gouns";
 import {mens_kurta} from "../../../../../Data/Men/men_kurtts";
 import {mensShoesPage1} from "../../../../../Data/shoes";
 import {lengha_page1} from "../../../../../Data/Women/LenghaCholi";
+import {ProductService} from "../../../../State/Product/product.service";
+import {select, Store} from "@ngrx/store";
+import {AppState} from "../../../../Models/AppState";
 
 @Component({
   selector: 'app-home',
@@ -16,7 +19,8 @@ export class HomeComponent {
   lehengaCholi: any;
   mansKurta: any;
   shoes: any;
-
+constructor(private productService: ProductService, private store: Store<AppState>) {
+}
 ngOnInit(){
   this.manJeans = manJens.slice(0, 6);//i will do it later random ;
   this.womansGouns = gounsPage1.slice(0,6);
@@ -24,6 +28,29 @@ ngOnInit(){
   this.lehengaCholi = this.womansGouns;
   this.mansKurta = mens_kurta.slice(0,6);
   this.shoes = mensShoesPage1.slice(0,6);
+
+
+  this.extracted('women_dress');
 }
 
+  private extracted(category: string) {
+    let reqData = {
+      category: category,
+      colors: [],
+      sizes: [],
+      minPrice: 0,
+      maxPrice: 1000000000,
+      minDiscount: 0,
+      pageNumber: 0,
+      pageSize: 6,
+      sort: 'price_low',
+      stock: "",
+    }
+    this.productService.findProductByFilterService(reqData);
+    this.store.pipe(select((store: AppState) => store.product)).subscribe((product) => {
+      if(product.content.content.length)
+        this.womansGouns = product.content;
+      console.log("from home page", product)
+    })
+  }
 }
